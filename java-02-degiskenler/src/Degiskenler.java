e4b31e31f0303c6206df3e2eb3c56984b0c4d36f
aydin <aydin@AYDIN>
2025-08-16 16:42:05 +0300

    Add java-02-degiskenler project

---
+++ java-02-degiskenler/src/Degiskenler.java
@@ -0,0 +1,65 @@
+
+public class Degiskenler {
+	
+//	çay kasığı
+//	çorba kasığı
+//	kepçe
+//	tabak
+//	bardak
+//	tencere
+//	kazan
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int myInt = 10;
+		byte myByte = 20;
+		short myShort = 30;
+		long myLong = 40;
+		float myFloat = 50.12345671234567f;
+		double myDouble = 60.12345678901234567891234567890123456789;
+		char myChar = 'A';
+		boolean myBoolean = true;
+		
+		System.out.println(myInt);
+		
+		
+		myInt = 1000000000;
+		System.out.println(myInt);
+		System.out.println(myByte);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+		System.out.println("------------------------------------");
+		
+		
+		
+		Integer myInt2 = 10;
+		Byte myByte2 = 20;
+		Short myShort2 = 30;
+		Long myLong2 = 40L;
+		Float myFloat2 = 50.12345671234567f;
+		Double myDouble2 = 60.12345678901234567891234567890123456789;
+		Character myChar2 = 'A';
+		Boolean myBoolean2 = true;
+		
+		System.out.println(myInt2);
+		System.out.println(myByte2);
+		System.out.println(myShort2);
+		System.out.println(myLong2);
+		System.out.println(myFloat2);
+		System.out.println(myDouble2);
+		System.out.println(myChar2);
+		System.out.println(myBoolean2);
+		
+
+	}
+
+}
---
+++ java-02-degiskenler/src/Insan.java
@@ -0,0 +1,98 @@
+
+public class Insan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String firstName = "Alp Arda";
+		String lastName = "Aras";
+		Integer age = 24;
+        Float height = 1.80f;
+		Double salary = 120_000.0;
+		Boolean maritalStatus  = true;
+		Character gender = 'M';
+		String eyesColor = "Blue";
+		
+		System.out.println("1\n ---------------------------------------");
+		
+		System.out.println("firstName : " + firstName);
+		System.out.println("lastName : " + lastName);
+		System.out.println("Age : " + age);
+		System.out.println("height : " + height);
+		System.out.println("salary : "+ salary);
+		System.out.println("maritalStatus : "+ maritalStatus);
+		System.out.println("gender : "+ gender);
+		System.out.println("eyesColor : "+ eyesColor);
+		
+		System.out.println("2\n ---------------------------------------");
+		
+		
+
+		 firstName = "Abdullah";
+		 lastName = "Gülbaz";
+		 age = 26;
+         height = 1.70f;
+		 salary = 110_000.0;
+		 maritalStatus = true;
+		 gender = 'M';
+		 eyesColor = "Black";
+		 
+		System.out.println("firstName : " + firstName);
+		System.out.println("lastName : " + lastName);
+		System.out.println("Age : " + age);
+		System.out.println("height : " + height);
+		System.out.println("salary : "+ salary);
+        System.out.println("maritalStatus : "+ maritalStatus);
+        System.out.println("gender : "+ gender);
+        System.out.println("eyesColor : "+ eyesColor);
+		
+		
+		
+		System.out.println("3\n ---------------------------------------");
+		
+		
+
+		 firstName = "Celalettin";
+		 lastName = "Aksoy";
+		 age = 37;
+         height = 1.90f;
+		 salary = 130_000.0;
+		 maritalStatus = Boolean.FALSE;
+		 gender = 'M';
+		 eyesColor = "Green";
+		 
+		System.out.println("firstName : " + firstName);
+		System.out.println("lastName : " + lastName);
+		System.out.println("Age : " + age);
+		System.out.println("height : " + height);
+		System.out.println("salary : "+ salary);
+        System.out.println("maritalStatus : "+ maritalStatus);
+        System.out.println("gender : "+ gender);
+        System.out.println("eyesColor : "+ eyesColor);
+        
+        System.out.println("4\n ---------------------------------------");
+		
+		
+
+		 firstName = "Betül";
+		 lastName = "Sarıkaya";
+		 age = 18;
+        height = 1.70f;
+		 salary = 135_000.0;
+		 maritalStatus = Boolean.FALSE;
+		 gender = 'F';
+		 eyesColor = "Green";
+		 
+		System.out.println("firstName : " + firstName);
+		System.out.println("lastName : " + lastName);
+		System.out.println("Age : " + age);
+		System.out.println("height : " + height);
+		System.out.println("salary : "+ salary);
+       System.out.println("maritalStatus : "+ maritalStatus);
+       System.out.println("gender : "+ gender);
+       System.out.println("eyesColor : "+ eyesColor);
+		
+
+	}
+
+}
